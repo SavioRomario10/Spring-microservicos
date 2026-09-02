e11e06d3fc9f8a0cd34e6cbc9f05547473340338
Sávio Romário <savioromario10@gmail.com>
2026-09-02 14:48:05 -0300

    first commit

---
+++ eurekaserver/src/test/java/io/savioromario10/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package io.savioromario10.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
