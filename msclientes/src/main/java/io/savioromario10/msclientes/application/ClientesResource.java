701e9674dc3a0010c0e55b2e5cac1b17592112ac
Sávio Romário <savioromario10@gmail.com>
2026-09-03 16:10:59 -0300

    adicionando um gateway para gerenciar os microserviços

--- msclientes/src/main/java/io/savioromario10/msclientes/application/ClientesResource.java
+++ msclientes/src/main/java/io/savioromario10/msclientes/application/ClientesResource.java
@@ -1,13 +1,26 @@
 package io.savioromario10.msclientes.application;
 
+import java.net.URI;
+import lombok.RequiredArgsConstructor;
+
+import io.savioromario10.msclientes.domain.Cliente;
+import io.savioromario10.msclientes.application.representation.ClienteSaveRequest;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 @RestController
 @RequestMapping("clientes")
+@RequiredArgsConstructor
 public class ClientesResource {
+
+  private final ClienteService service;
 
   @GetMapping
   public String status() {
@@ -15,7 +28,29 @@ public class ClientesResource {
   }
 
   @PostMapping
-  public void save() {
+  public ResponseEntity<?> save(@RequestBody ClienteSaveRequest request) {
 
+    Cliente cliente = request.toModel();
+    service.save(cliente);
+
+    URI headerLocation = ServletUriComponentsBuilder
+        .fromCurrentRequest()
+        .query("cpf={cpf}")
+        .buildAndExpand(cliente.getCpf())
+        .toUri();
+
+    return ResponseEntity.created(headerLocation).build();
+  }
+
+  @GetMapping(params = "cpf")
+  public ResponseEntity<?> dadosCliente(@RequestParam("cpf") String cpf) {
+
+    var cliente = service.getByCpf(cpf);
+
+    if (cliente.isEmpty()) {
+      return ResponseEntity.notFound().build();
+    }
+
+    return ResponseEntity.ok(cliente.get());
   }
 }
---
+++ mscloudgateway/src/test/java/io/savioromario10/mscloudgateway/MscloudgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package io.savioromario10.mscloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscloudgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
